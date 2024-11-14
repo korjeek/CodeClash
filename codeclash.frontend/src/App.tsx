b0d10f59de3e6264b47e5cba63a755d205780db6
Unknown030405 <dpsdanilpar@gmail.com>
2024-11-14 14:41:37 -0600

    first work on frontend

--- codeclash.frontend/src/App.tsx
+++ codeclash.frontend/src/App.tsx
@@ -1,7 +1,37 @@
-import Message from "./Message.tsx";
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
 
-function App(){
-    return <div><Message /></div>
-}
+const App: React.FC = () => {
+  return (
+//     <div>
+//             <Register />
+//     </div>
+//   );
+        <Router>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">App</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Log in</Link>
+                </li>
+            </ul>
+        </nav>
+
+        <Routes>
+            <Route path="/" element={App} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
+        </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ codeclash.frontend/src/services/authService.tsx
@@ -0,0 +1,26 @@
+// src/services/authService.ts
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5099/api/auth';
+
+export interface RegisterUser {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export interface LoginUser {
+  username: string;
+  password: string;
+}
+
+export const register = async (userData: RegisterUser) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+export const login = async (userData: LoginUser) => {
+  const response = await axios.post(`${API_URL}/login`, userData);
+  return response.data;
+};
