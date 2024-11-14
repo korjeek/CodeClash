db4ff1449cc70387a38fbe01fe8610e06cf0eb59
artemchick35 <artemryzhkov2004@gmail.com>
2024-11-14 16:34:48 +0500

    TryAddRooms

---
+++ CodeClash.Core/Entities/Room.cs
@@ -0,0 +1,45 @@
+﻿using CodeClash.Core.Models;
+
+namespace CodeClash.Core.Entities;
+
+public class Room(Guid id, List<Problem> problems, Room.RoomStatus status)
+{
+    public Guid Id { get; } = id;
+    public List<Problem> Problems { get; } = problems;
+    private RoomStatus Status { get; set; } = status;
+    private List<User> Participants { get; } = [];
+    private Competition _competition = new();
+    
+    public void AddParticipant(User participant)
+    {
+        if (Status is RoomStatus.WaitingForParticipants)
+            Participants.Add(participant);
+        else
+            throw new InvalidOperationException("Cannot add participant. Competition in progress!");
+    }
+    
+    public void StartCompetition()
+    {
+        if (Status is RoomStatus.WaitingForParticipants)
+        {
+            Status = RoomStatus.CompetitionInProgress;
+            _competition.Start();
+        }
+        else 
+            throw new InvalidOperationException("Competition is already started!");
+    }
+    
+    public void FinishCompetition()
+    {
+        if (Status is RoomStatus.CompetitionInProgress)
+            Status = RoomStatus.WaitingForParticipants;
+        else
+            throw new InvalidOperationException("Competition is not started!");
+    }
+    
+    public enum RoomStatus
+    {
+        WaitingForParticipants = 0,
+        CompetitionInProgress = 1
+    }
+}
---
+++ CodeClash.Core/Models/Problem.cs
@@ -0,0 +1,5 @@
+﻿namespace CodeClash.Core.Models;
+
+
+// подумать над тем, какой тип тестовых данных использовать
+public record Problem(Guid Id, string Description, Dictionary<Guid, (int[], int[])> TestData);
