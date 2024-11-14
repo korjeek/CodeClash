a7ea173db66603717e88621135e44283b5519002
korjeek <olegka823@gmail.com>
2024-11-15 01:04:04 +0500

    auth controllers update

--- CodeClash.API/Services/AuthService.cs
+++ CodeClash.API/Services/AuthService.cs
@@ -1,18 +1,47 @@
-﻿using CodeClash.Persistence.Repositories;
+﻿using CodeClash.Application;
+using CodeClash.Core.Models;
+using CodeClash.Core.Models.Identity;
+using CodeClash.Core.Services;
+using CodeClash.Persistence.Repositories;
 
-namespace CodeClash.Application.Services;
+namespace CodeClash.API.Services;
 
-public class AuthService(UsersRepository usersRepository, PasswordHasher passwordHasher)
+public class AuthService(UsersRepository usersRepository, PasswordHasher passwordHasher, TokenService tokenService)
 {
-    public async Task<bool> IsLoginValid(string email, string password)
+    public async Task<User?> GetUser(LoginRequest request)
     {
-        var managedUser = await usersRepository.FindUserByEmail(email);
-        if (managedUser == null)
-            return false;
+        var user = await usersRepository.FindUserByEmail(request.Email);
+        if (user is null)
+            return null;
         
-        var managedPassword = await usersRepository.GetPassword(managedUser.Id);
-        return passwordHasher.Verify(password, managedPassword);
+        var password = await usersRepository.GetPassword(user.Id);
+        return passwordHasher.Verify(password, user.PasswordHash) ? null : user;
     }
-    
-    public async Task<string> HashPassword(string password) => passwordHasher.Generate(password);
+
+    public async Task<User?> CreateUser(RegisterRequest request)
+    {
+        var passwordHash = passwordHasher.Generate(request.Password);
+        return await usersRepository.AddUser(new User(request.UserName, request.Email, passwordHash));
+    }
+
+    public async Task<User?> GetUserByPrincipalClaims(JwtToken tokenModel)
+    {
+        var principal = tokenService.GetPrincipalClaims(tokenModel.AccessToken);
+        return principal is null ? null : await usersRepository.FindUserByUserName(principal.Identity!.Name);
+    }
+
+    public JwtToken UpdateUsersTokens(User user)
+    {
+        var tokens = tokenService.UpdateTokens(user);
+        usersRepository.UpdateUsersRefreshToken(user.Id, tokens.RefreshToken);
+        UpdateUsersRefreshTokenProperties(user, tokens.RefreshToken);
+        
+        return tokens;
+    }
+
+    private void UpdateUsersRefreshTokenProperties(User user, string refreshToken)
+    {
+        user.RefreshToken = refreshToken;
+        user.RefreshTokenExpiryTime = DateTime.Now.AddDays(7);
+    }
 }
