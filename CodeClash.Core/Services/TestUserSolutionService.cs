fac329bf9697f9fc2cc19bf2af1b7cd4a170ec81
artemchick35 <artemryzhkov2004@gmail.com>
2024-12-05 22:50:35 +0500

    dont repeat yourself

--- CodeClash.Core/Services/TestUserSolutionService.cs
+++ CodeClash.Core/Services/TestUserSolutionService.cs
@@ -4,14 +4,13 @@ namespace CodeClash.Core.Services;
 
 public class TestUserSolutionService()
 {
-    public Task<Answer> CheckSolution(string userSolution)
+    public async Task CheckSolution(string userSolution)
     {
         // TODO: как добавить общий том для контейнеров?
         // TODO: как обратиться к контейнеру?
         // TODO: как сбилдить проект?
         // TODO: как считать вывод?
         File.WriteAllText("CodeClash.UserSolutionTest/SolutionTask.cs", userSolution);
-
-        throw new NotImplementedException();
+        
     }
 }
