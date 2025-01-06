7c0436736b2f5d7faccaf1bbb60584dc12230fdb
korjeek <olegka823@gmail.com>
2025-01-06 21:50:58 +0500

    dirs update + links fixes

--- codeclash.frontend/src/services/RoomService.ts
+++ codeclash.frontend/src/services/RoomService.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import {Issue} from "../interfaces/issueInterfaces.ts";
-import {Room} from "../interfaces/roomInterfaces.ts";
+import {Room} from "../interfaces/RoomInterfaces.ts";
 
 const API_URL = 'https://localhost:7282/rooms';
 
@@ -11,5 +10,10 @@ export const checkForAdmin = async (): Promise<boolean> => {
 
 export const getRoom = async (): Promise<Room> => {
     const response = await axios.get(`${API_URL}/get-room-info`, { withCredentials: true });
+    return response.data.data;
+}
+
+export const getRoomsList = async (): Promise<Room[]> => {
+    const response = await axios.get(`${API_URL}/get-rooms`, { withCredentials: true });
     return response.data.data;
 }
